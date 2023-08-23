ac5d58e85bed63cb9b96972e27a9c16ac266018d
leandro <leandro262009@gmail.com>
2023-08-23 11:25:11 +0200

    Started home page

--- ui/src/layouts/homelayout.rs
+++ ui/src/layouts/homelayout.rs
@@ -10,8 +10,13 @@ pub fn HomeLayout(props: &HomeLayoutProps) -> Html {
     let HomeLayoutProps { children } = props;
 
     html! {
-        <div>
-            <header></header>
+        <div class="font-bebas">
+            <header class="h-16 bg-nut flex items-center">
+                <a href="/" class="flex flex-row ml-5 transition-all hover:tracking-widest">
+                    <h1 class="mr-5 text-5xl">{"Tournoix"}</h1>
+                    <img src="/img/nut.svg" class="h-10"/>
+                </a>
+            </header>
 
             <main class={"w-full px-5"}>
                 {children.clone()}
--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -7,8 +7,22 @@ module.exports = {
     "./src/**/*.css",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bebas: ['Bebas Neue', 'sans-serif'],
+      },
+
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        highlight: '#ACEDFF',
+        nut: '#F4D1AE',
+        dark: '#191923',
+        primary: '#89BBFE',
+        secondary: '#6F8AB7',
+        discrete: '#BBB9B6',
+      },
+    },
   },
   plugins: [],
 }
-
