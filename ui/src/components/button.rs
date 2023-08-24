6239ab6235db570ba4604ac9ab7baca29a183083
Fuler <dorian.niclass@gmail.com>
2023-08-24 15:31:26 +0200

    sticky header

--- ui/src/components/button.rs
+++ ui/src/components/button.rs
@@ -4,15 +4,15 @@ use yew::prelude::*;
 pub struct ButtonProps {
     #[prop_or_default]
     pub class: Classes,
-    pub label: String,
+    pub children: Children,
     pub onclick: Callback<MouseEvent>,
 }
 
 #[function_component]
 pub fn Button(props: &ButtonProps) -> Html {
-    let ButtonProps { class, label, onclick } = props;
+    let ButtonProps { class, children, onclick } = props;
 
     html! {
-        <button class={classes!(class.clone(), "bg-nut", "text-white", "hover:tracking-[.05em]", "rounded", "border", "drop-shadow-md", "hover:scale-125", "hover:duration-[200ms]", "duration-[600ms]", "transition-all")} onclick={onclick}>{label}</button>
+        <button class={classes!(class.clone(), "bg-nut", "text-white", "hover:tracking-[.05em]", "rounded", "border", "drop-shadow-md", "hover:scale-125", "hover:duration-[200ms]", "duration-[600ms]", "transition-all")} onclick={onclick}>{children.clone()}</button>
     }
 }
--- ui/src/pages/tournoix.rs
+++ ui/src/pages/tournoix.rs
@@ -1,7 +1,6 @@
 use yew::prelude::*;
-use yew_router::prelude::use_navigator;
 
-use crate::routers::Route;
+use crate::layouts::homelayout::HomeLayout;
 
 #[derive(PartialEq, Properties)]
 pub struct TournoixProps {}
@@ -9,13 +8,12 @@ pub struct TournoixProps {}
 #[function_component]
 pub fn Tournoix(props: &TournoixProps) -> Html {
     let TournoixProps {} = props;
-    let navigator = use_navigator().unwrap();
     
-    let onclick = Callback::from(move |_| navigator.push(&Route::Home));
     html! {
-        <div class={"flex flex-col"}>
-            {"Tournoixxx"}
-            <button {onclick}>{"Go to Home"}</button>
-        </div>
+        <HomeLayout>
+            <div class={"flex flex-col p-4"}>
+                {"Come back later"}
+            </div>
+        </HomeLayout>
     }
 }
