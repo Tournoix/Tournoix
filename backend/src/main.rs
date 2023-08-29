faaf1a78a553f489a0665c3730544b23a4a9f264
rhyan <rhyan.robertson@phrengames.ch>
2023-08-29 14:22:50 +0200

    Add route

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,7 +9,7 @@ use std::path::{Path, PathBuf};
 use dotenv::dotenv;
 use rocket::{fs::NamedFile, http::Status, response::status::NotFound, serde::json::Json};
 use rocket_sync_db_pools::database;
-use routes::users::get_user;
+use routes::{users::get_user, auth::login};
 use serde::Serialize;
 
 mod routes;
@@ -34,7 +34,7 @@ fn rocket() -> _ {
     rocket::build()
         .attach(MysqlConnection::fairing())
         .mount("/", routes![index, static_file])
-        .mount("/api", routes![get_user, api_hole])
+        .mount("/api", routes![get_user, login, api_hole])
 }
 
 async fn get_index() -> Result<NamedFile, NotFound<String>> {
