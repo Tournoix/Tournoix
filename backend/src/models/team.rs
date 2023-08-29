ca2f877399b0a3f2b1556a5b4b1a482d04b55e6b
JorisDecoppetHeigVd <joris.decoppet@heig-vd.ch>
2023-08-29 16:05:30 +0200

    CRUD api team

--- backend/src/models/team.rs
+++ backend/src/models/team.rs
@@ -11,11 +11,19 @@ pub struct Team {
     pub group: i32,
 }
 
-#[derive(Serialize, Deserialize, Insertable)]
+#[derive(Serialize, Deserialize, Insertable, Clone)]
 #[diesel(belongs_to(Tournament))]
 #[diesel(table_name = teams)]
 pub struct NewTeam {
     pub fk_tournaments: i32,
     pub name: String,
     pub group: i32,
+}
+
+#[derive(Serialize, Deserialize, Insertable, Clone)]
+#[diesel(belongs_to(Tournament))]
+#[diesel(table_name = teams)]
+pub struct PatchTeam {
+    pub name: Option<String>,
+    pub group: Option<i32>,
 }
