71df092a82cb387f27f3c82f1ff204bc65c9ffa3
rhyan <rhyan.robertson@heig-vd.ch>
2023-08-30 10:07:39 +0200

    Fix merge inot dev

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -12,7 +12,6 @@ use rocket_sync_db_pools::database;
 use routes::{users::get_user, auth::{login, logout, register}};
 use serde::Serialize;
 
-use crate::routes::users::get_user;
 use crate::routes::tournoix::*;
 use crate::routes::team::*;
 
