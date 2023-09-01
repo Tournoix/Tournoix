dc1a5e5c2acb00015342794d809fe6e782bf5cf3
Fuler <dorian.niclass@gmail.com>
2023-09-01 10:25:55 +0200

    fix logout route

--- backend/src/routes/auth.rs
+++ backend/src/routes/auth.rs
@@ -52,7 +52,7 @@ impl<'r> FromRequest<'r> for ApiAuth {
                 {
                     Ok(token) => {
                         if token.expiration_date < chrono::Local::now().naive_local() {
-                            // Token is expired => 403 and delete the token
+                            // Token is expired => 401 and delete the token
                             connection
                                 .run(move |c| {
                                     diesel::delete(tokens::table.find(token.token)).execute(c)
@@ -81,7 +81,7 @@ impl<'r> FromRequest<'r> for ApiAuth {
                             }),
 
                             Err(_e) => {
-                                // Erro while getting the user linked to the token => 403
+                                // Erro while getting the user linked to the token => 401
                                 Outcome::Failure((
                                     Status::Forbidden,
                                     ApiAuthResponse::Unauthorized("Invalid token".to_string()),
@@ -91,7 +91,7 @@ impl<'r> FromRequest<'r> for ApiAuth {
                     }
 
                     Err(_e) => {
-                        // Error while getting the token (either it doesn't exist or DB related error) => 403
+                        // Error while getting the token (either it doesn't exist or DB related error) => 401
                         Outcome::Failure((
                             Status::Forbidden,
                             ApiAuthResponse::Unauthorized("Invalid token".to_string()),
@@ -175,101 +175,23 @@ pub async fn login(
     }
 }
 
-/* #[post("/auth/login", data = "<data>")]
-pub async fn login(
-    connection: MysqlConnection,
-    data: Json<LoginRequest>,
-) -> Result<Json<LoginResponse>, (Status, String)> {
-    // Find user by email
-    match connection
-        .run({
-            let data = data.clone();
-            move |c| users::table.filter(email.eq(&data.email)).first::<User>(c)
-        })
-        .await
-    {
-        Ok(user) => {
-            // Check if password is correct
-            if crypto::verify_password(&user.password, &data.password) {
-                // Generate token
-                let token_string = crypto::generate_token();
-
-                // Add token to db
-                let token = NewToken {
-                    token: token_string,
-                    fk_users: user.id,
-                    expiration_date: (chrono::offset::Local::now() + Duration::hours(3))
-                        .naive_local(),
-                };
-
-                match connection
-                    .run({
-                        let token = token.clone();
-                        move |c| insert_into(tokens::dsl::tokens).values(token).execute(c)
-                    })
-                    .await
-                {
-                    Ok(_) => {
-                        let reponse = LoginResponse {
-                            token: token.token,
-                            expiration_date: token.expiration_date,
-                        };
-
-                        // Return token
-                        return Ok(Json(reponse));
-                    }
-
-                    Err(e) => return Err((Status::InternalServerError, e.to_string())),
-                }
-            } else {
-                return Err((Status::Unauthorized, "incorrect login".to_string()));
-            }
-        }
-
-        Err(_e) => return Err((Status::Unauthorized, "incorrect login".to_string())),
-    }
-} */
-
 // Logout user
 #[derive(Deserialize, Clone)]
 pub struct LogoutRequest {
     pub token: String,
 }
 
-/* #[post("/auth/logout", data = "<data>")]
+#[post("/auth/logout")]
 pub async fn logout(
     connection: MysqlConnection,
-    data: Json<LogoutRequest>,
+    auth: ApiAuth,
 ) -> Result<Json<()>, (Status, String)> {
     // Delete token from db
     match connection
-        .run({
-            let data = data.clone();
-            move |c| diesel::delete(tokens::table.filter(tokens::token.eq(&data.token))).execute(c)
-        })
+        .run(move |c| diesel::delete(tokens::table.find(auth.token)).execute(c))
         .await
     {
         Ok(_) => return Ok(Json(())),
-
-        Err(e) => return Err((Status::InternalServerError, e.to_string())),
-    }
-} */
-
-#[post("/auth/logout", data = "<data>")]
-pub async fn logout(
-    connection: MysqlConnection,
-    data: Json<LogoutRequest>,
-) -> Result<Json<()>, (Status, String)> {
-    // Delete token from db
-    match connection
-        .run({
-            let data = data.clone();
-            move |c| diesel::delete(tokens::table.filter(tokens::token.eq(&data.token))).execute(c)
-        })
-        .await
-    {
-        Ok(_) => return Ok(Json(())),
-
         Err(e) => return Err((Status::InternalServerError, e.to_string())),
     }
 }
