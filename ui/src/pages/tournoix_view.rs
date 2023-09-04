c3b518d022ded1079d9925fd0ccf7efa9425a0d7
leandro <leandro262009@gmail.com>
2023-09-04 16:02:32 +0200

    Added list with link to available matches to bet

--- ui/src/pages/tournoix_view.rs
+++ ui/src/pages/tournoix_view.rs
@@ -1,7 +1,7 @@
 use yew::prelude::*;
 use yew_router::prelude::use_navigator;
 
-use crate::{layouts::homelayout::HomeLayout, routers::Route, components::{backlink::Backlink, results::Results, qualificationPhase::QualificationPhase, bracket::Bracket, join_code::JoinCode, groups::{Groups, Group}}, utils::utils::fetch_tournament};
+use crate::{layouts::homelayout::HomeLayout, routers::Route, components::{backlink::Backlink, results::Results, qualificationPhase::QualificationPhase, bracket::{Bracket, Match}, join_code::JoinCode, groups::{Groups, Group}, bet_list::BetList}, utils::utils::fetch_tournament};
 
 #[derive(PartialEq, Properties)]
 pub struct TournoixViewProps {
@@ -17,6 +17,46 @@ pub fn TournoixView(props: &TournoixViewProps) -> Html {
 
     // TODO Wheter or not the current user can edit this tournament
     let can_edit_tournament = true;
+    let user_nut = 20;
+
+    let matches: UseStateHandle<Vec<Match>> = use_state(|| vec![
+        Match {
+            id: 0,
+            team1: "Cloud9".to_string(),
+            score1: 0,
+            team2: "FaZe Clan".to_string(),
+            score2: 0,
+            started: false,
+            finished: false
+        },
+        Match {
+            id: 1,
+            team1: "NaVi".to_string(),
+            score1: 0,
+            team2: "NRG Esports".to_string(),
+            score2: 0,
+            started: true,
+            finished: false
+        },
+        Match {
+            id: 2,
+            team1: "G2 Esports".to_string(),
+            score1: 0,
+            team2: "fnatic".to_string(),
+            score2: 0,
+            started: true,
+            finished: true
+        },
+        Match {
+            id: 3,
+            team1: "Team with a comically long name".to_string(),
+            score1: 0,
+            team2: "Team 42".to_string(),
+            score2: 0,
+            started: false,
+            finished: false
+        }
+    ]);
 
     let groups: UseStateHandle<Vec<Group>> = use_state(|| vec![
         Group { },
@@ -52,11 +92,16 @@ pub fn TournoixView(props: &TournoixViewProps) -> Html {
                 <div>{"Lieu: "}{tournament.location.to_string()}</div>
                 <div>{"Description: "}{tournament.description.to_string()}</div>
                 <hr/>
+                <h2>{"Paris disponibles"}</h2>
+                <p class="discrete">{"Vous pouvez misez vos noix dans ces matchs et peut-être remporter le pactole !"}</p>
+                <p class="mb-4">{format!("Vous possédez actuellement {} noix.", user_nut)}</p>
+                <BetList matches={(*matches).clone()}/>
+                <hr/>
                 <h2>{"Phase de qualifications"}</h2>
                 <ContextProvider<UseStateHandle<Vec<Group>>> context={groups.clone()}>
                     <Groups/>
                 </ContextProvider<UseStateHandle<Vec<Group>>>>
-                <QualificationPhase/>
+                /*<QualificationPhase/>*/
                 <hr/>
                 <h2>{"Phase d'éliminations"}</h2>
                 /*<Bracket/>*/
