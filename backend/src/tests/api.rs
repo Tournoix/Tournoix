85c986a17aad347f96d0067b28994c182de5893a
rhyan <rhyan.robertson@heig-vd.ch>
2023-09-07 23:46:10 +0200

    FIX ?

--- backend/src/tests/api.rs
+++ backend/src/tests/api.rs
@@ -43,12 +43,13 @@ fn successful_register_request() {
 }
 
 #[test]
-fn successful_login_logoff_request() {
+fn successful_login_logoff_request(){
     use crate::routes::auth::LoginRequest;
     use rocket::http::Header;
     use crate::models::user::NewUser;
 
 
+    std::thread::sleep(std::time::Duration::from_secs(3));
 
     const TEST_USER_EMAIL: &str = "john.doe2@tournoix.com";
     const TEST_USER_PASSWORD: &str ="Password123!2";
@@ -56,18 +57,11 @@ fn successful_login_logoff_request() {
 
     let c = client();
 
-    // Register user
-    let register_request = NewUser {
-        email: TEST_USER_EMAIL.to_owned(),
-        password: TEST_USER_PASSWORD.to_owned(),
-        name: TEST_USER_NAME.to_owned()
-    };
-
-    let json_register_request = serde_json::to_string(&register_request);
+    let json_register_request = "{\"email\":\"TEST_USER_EMAIL\",\"password\":\"TEST_USER_PASSWORD\",\"name\":\"TEST_USER_NAME\"}";
 
     let response = c.post("/api/auth/register")
         .header(ContentType::JSON)
-        .body(json_register_request.unwrap())
+        .body(json_register_request)
         .dispatch();
 
     // Wait for user to be registered
@@ -76,32 +70,27 @@ fn successful_login_logoff_request() {
 
     info!("Register Response: {:?}", response);
     assert_eq!(response.status(), Status::Ok);
-
     
-    let login_request = LoginRequest {
-        email: TEST_USER_EMAIL.to_owned(),
-        password: TEST_USER_PASSWORD.to_owned() 
-    };
-    
-    let json_login_request = serde_json::to_string(&login_request);
-
-
+    let json_login_request = "{\"email\":\"TEST_USER_EMAIL\",\"password\":\"TEST_USER_PASSWORD\"}";
 
     let response = c.post("/api/auth/login")
         .header(ContentType::JSON)
-        .body(json_login_request.unwrap())
+        .body(json_login_request)
         .dispatch();
 
     info!("Login Response: {:?}", response);
     assert_eq!(response.status(), Status::Ok);
 
     // Save token from response
-    let login_response: LoginResponse = serde_json::from_str(&response.into_string().unwrap()).unwrap();
+    // "{\"token\":\"f35e9be6-db56-4e0a-8d26-82abb507e828\",\"expiration_date\":\"2023-09-08T02:41:49.201130\"}"
+    let token = response.into_string().unwrap().split(":").nth(1).unwrap().split(",").nth(0).unwrap().replace("\"", "");
     
-    let token = login_response.token;
     info!("Token: {:?}", token);
 
-    let mut headers = Header::new("Authorization", format!("Bearer {}", token));
+    let headers = Header::new("Authorization", format!("Bearer {}", token));
+
+    std::thread::sleep(std::time::Duration::from_secs(3));
+
 
     let response = c.post("/api/auth/logout")
         .header(ContentType::JSON)
